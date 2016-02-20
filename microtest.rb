0d4ec08de4d7368996d423c9f1e901262b914e13
Ryan Davis <ryand-ruby@zenspider.com>
2016-02-20 15:11:44 -0800

    Added assert_equal

--- microtest.rb
+++ microtest.rb
@@ -1,3 +1,7 @@
 def assert test
   raise RuntimeError, "Failed test", caller unless test
 end
+
+def assert_equal a, b
+  assert a == b
+end
--- test.rb
+++ test.rb
@@ -2,4 +2,8 @@ require "./microtest.rb"
 
 assert true
 
-assert false
+# assert false
+
+assert_equal 4, 2+2
+
+assert_equal 5, 2+2
