5133f90fa9f218457b14d69e313c0fe353cea439
Ryan Davis <ryand-ruby@zenspider.com>
2016-02-20 15:47:02 -0800

    Wrapped tests in a class and manually instantiate and run

--- test.rb
+++ test.rb
@@ -7,24 +7,26 @@ rescue
   # good, we should have raised
 end
 
-def test_assert
-  assert true
+class TestAssertions
+  def test_assert
+    assert true
 
-  should_fail { assert false }
+    should_fail { assert false }
+  end
+
+  def test_assert_equal
+    assert_equal 4, 2+2
+
+    should_fail { assert_equal 5, 2+2 }
+  end
+
+  def test_assert_in_delta
+    assert_in_delta 0.0001, 0.0002
+
+    should_fail { assert_in_delta 0.5, 0.6 }
+  end
 end
 
-def test_assert_equal
-  assert_equal 4, 2+2
-
-  should_fail { assert_equal 5, 2+2 }
-end
-
-def test_assert_in_delta
-  assert_in_delta 0.0001, 0.0002
-
-  should_fail { assert_in_delta 0.5, 0.6 }
-end
-
-test_assert
-test_assert_equal
-test_assert_in_delta
+TestAssertions.new.test_assert
+TestAssertions.new.test_assert_equal
+TestAssertions.new.test_assert_in_delta
