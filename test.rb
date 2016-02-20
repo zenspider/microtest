862f22572d7488994b935cc82e7686f391615f6b
Ryan Davis <ryand-ruby@zenspider.com>
2016-02-20 15:12:55 -0800

    Wrote should_fail to allow tests to keep running w/ failed assertions

--- test.rb
+++ test.rb
@@ -1,9 +1,16 @@
 require "./microtest.rb"
 
+def should_fail
+  yield
+  abort "BAD RUN!"
+rescue
+  # good, we should have raised
+end
+
 assert true
 
-# assert false
+should_fail { assert false }
 
 assert_equal 4, 2+2
 
-assert_equal 5, 2+2
+should_fail { assert_equal 5, 2+2 }
