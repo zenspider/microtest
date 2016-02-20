a38c1458c71ac1655629a1732e258517a43ae33a
Ryan Davis <ryand-ruby@zenspider.com>
2016-02-20 15:41:10 -0800

    Added assert_in_delta.

--- microtest.rb
+++ microtest.rb
@@ -8,3 +8,7 @@ end
 def assert_equal a, b
   assert a == b, "Failed assert_equal #{a} vs #{b}"
 end
+
+def assert_in_delta a, b
+  assert (a-b).abs <= 0.001, "Failed assert_in_delta #{a} vs #{b}"
+end
--- test.rb
+++ test.rb
@@ -14,3 +14,7 @@ should_fail { assert false }
 assert_equal 4, 2+2
 
 should_fail { assert_equal 5, 2+2 }
+
+assert_in_delta 0.0001, 0.0002
+
+should_fail { assert_in_delta 0.5, 0.6 }
