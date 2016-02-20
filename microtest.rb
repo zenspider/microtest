88541c57134a78e938a439cdcf31e0fc5d1d2b35
Ryan Davis <ryand-ruby@zenspider.com>
2016-02-20 15:15:24 -0800

    Add ability to specify failed assertion message. Switch assert_equal

--- microtest.rb
+++ microtest.rb
@@ -1,10 +1,10 @@
-def assert test
+def assert test, msg = "Failed test"
   unless test then
     backtrace = caller.drop_while { |s| s =~ /#{__FILE__}/ }
-    raise RuntimeError, "Failed test", backtrace
+    raise RuntimeError, msg, backtrace
   end
 end
 
 def assert_equal a, b
-  assert a == b
+  assert a == b, "Failed assert_equal #{a} vs #{b}"
 end
