47fe75832cae9d5b634624ac70c38ecde3d1669c
Ryan Davis <ryand-ruby@zenspider.com>
2016-02-20 16:18:49 -0800

    Wrap assertions into a module. Include in Test.

--- microtest.rb
+++ microtest.rb
@@ -1,19 +1,23 @@
-def assert test, msg = "Failed test"
-  unless test then
-    backtrace = caller.drop_while { |s| s =~ /#{__FILE__}/ }
-    raise RuntimeError, msg, backtrace
+module Assertions
+  def assert test, msg = "Failed test"
+    unless test then
+      backtrace = caller.drop_while { |s| s =~ /#{__FILE__}/ }
+      raise RuntimeError, msg, backtrace
+    end
+  end
+
+  def assert_equal a, b
+    assert a == b, "Failed assert_equal #{a} vs #{b}"
+  end
+
+  def assert_in_delta a, b
+    assert (a-b).abs <= 0.001, "Failed assert_in_delta #{a} vs #{b}"
   end
 end
 
-def assert_equal a, b
-  assert a == b, "Failed assert_equal #{a} vs #{b}"
-end
-
-def assert_in_delta a, b
-  assert (a-b).abs <= 0.001, "Failed assert_in_delta #{a} vs #{b}"
-end
-
 class Test
+  include Assertions
+
   def self.run
     public_instance_methods.grep(/^test/).each do |name|
       self.new.run name
